4bae5e18ce7a4068c5a7abf78143e8eedd9392d8
Everett <everett.bowline@gmail.com>
2018-03-07 03:24:25 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-do #display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-
+do #display_rainbow (display_rainbow['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts R: 
 end
